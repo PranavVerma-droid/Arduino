b8c71cc3af59809b6d2f4ec2370400ca436dd8a4
PranavVerma-droid <kumarpranav@verma.net.in>
2022-07-06 16:31:50 +0530

    Update

--- Parachute_System/Parachute_System.ino
+++ Parachute_System/Parachute_System.ino
@@ -6,6 +6,8 @@ const int buttonPin2 = 5;
 int buttonState1 = 0;
 int buttonState2 = 0;
 bool deploy = false;
+
+
 void setup()  
  {   
   Serial.begin(9600);
@@ -16,29 +18,29 @@ void setup()
  }  
 void loop()   
  {  
-  
-  
-  
-  
-  
-  
   buttonState1 = digitalRead(buttonPin1);
   buttonState2 = digitalRead(buttonPin2);
   if (buttonState1 == HIGH) {
       deploy = true;
-      digitalWrite(LED_BUILTIN, HIGH);
+      
        }
   if (buttonState2 == HIGH){
     deploy = false;
-    digitalWrite(LED_BUILTIN, LOW);
+    Serial.println("2nd Button Pressed");
+    
     
   }
-  if (deploy == true){
-    s1.write(180);
+  
+if (deploy == true){
+    delay(0);
+    s1.write(180);  
+    digitalWrite(LED_BUILTIN, HIGH);  
     Serial.println("Deploy = True");
   } else {
     s1.write(0);
-    Serial.println("Deploy = False"); }  
+    Serial.println("Deploy = False");
+    digitalWrite(LED_BUILTIN, LOW);
+    }  
  }
 
 
