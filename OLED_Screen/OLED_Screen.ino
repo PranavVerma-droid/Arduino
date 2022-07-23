d59b055128d905d52386c4f46817fb7478644f74
PranavVerma-droid <kumarpranav@verma.net.in>
2022-07-23 21:07:34 +0530

    Update

--- OLED_Screen/OLED_Screen.ino
+++ OLED_Screen/OLED_Screen.ino
@@ -4,9 +4,6 @@
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
-float h = 0;
-float t = 0;
-
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 //SCK PIN -> D1
 //SDA PIN -> D2
