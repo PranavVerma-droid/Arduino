5478599f12b671dd96920705c184c0e9a1faafdc
PranavVerma-droid <kumarpranav@verma.net.in>
2022-08-09 16:00:45 +0530

    Update Blink.ino

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -3,12 +3,15 @@
 void setup() {
  
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
 }
 
 
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  Serial.println("LED TURNED ON");
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  Serial.println("LED TURNED OFF");
   delay(1000);                       // wait for a second
 }
