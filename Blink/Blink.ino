2491142eb68a019190fd325ee5c084d66f7da38c
Pranav Verma <kumarpranav@verma.net.in>
2022-08-17 09:10:39 +0530

    Update Blink.ino

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,21 +1,16 @@
 int led = 43;
-
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(led, OUTPUT)
+  pinMode(LED_BUILTIN,OUTPUT);
+  pinMode(led,OUTPUT);
 }
-  
-
 void loop() {
+  digitalWrite(LED_BUILTIN,HIGH);
   digitalWrite(led,HIGH);
-  digitalWrite(LED_BUILTIN, HIGH);
 
   delay(1000);
 
-  digitalWrite(led,LOW);
   digitalWrite(LED_BUILTIN,LOW);
+  digitalWrite(led,LOW);
 
   delay(1000);
-
-
 }
