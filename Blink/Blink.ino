c4fecc3fc39c5b802c4033c15c1fcee0cadd295a
Pranav Verma <pranav@verma.net.in>
2022-10-29 20:08:33 +0530

    Update Blink.ino

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -5,12 +5,16 @@ void setup() {
 }
 void loop() {
   digitalWrite(LED_BUILTIN,HIGH);
+  
   digitalWrite(led,HIGH);
 
   delay(1000);
 
   digitalWrite(LED_BUILTIN,LOW);
+  
   digitalWrite(led,LOW);
+  //delay adding fo
 
   delay(1000);
+  //helpful for many reasons ofc llol
 }
