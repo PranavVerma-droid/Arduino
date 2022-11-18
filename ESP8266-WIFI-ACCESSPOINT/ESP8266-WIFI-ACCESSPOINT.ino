4fe33d088e89ab36ea10fe65e472111d4016b1c3
Pranav Verma <pranav@verma.net.in>
2022-11-18 10:36:47 +0530

    Update ESP8266-WIFI-ACCESSPOINT.ino

--- ESP8266-WIFI-ACCESSPOINT/ESP8266-WIFI-ACCESSPOINT.ino
+++ ESP8266-WIFI-ACCESSPOINT/ESP8266-WIFI-ACCESSPOINT.ino
@@ -1,7 +1,7 @@
 #include <ESP8266WiFi.h>        // Include the Wi-Fi library
 
-const char *ssid = "ESP8266 Access Point"; // The name of the Wi-Fi network that will be created
-const char *password = "thereisnospoon";   // The password required to connect to it, leave blank for an open network
+const char *ssid = "Pranav's Web Project 1"; // The name of the Wi-Fi network that will be created
+const char *password = "";   // The password required to connect to it, leave blank for an open network
 
 void setup() {
   Serial.begin(115200);
