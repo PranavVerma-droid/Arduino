7651cbe113030ac938570c7105a6eee9e6c29976
Pranav Verma <pranav@verma.net.in>
2022-11-28 15:29:02 +0530

    Update WIRELESS-WIFI-TEMPERATURE.ino

--- WIRELESS-WIFI-TEMPERATURE/WIRELESS-WIFI-TEMPERATURE.ino
+++ WIRELESS-WIFI-TEMPERATURE/WIRELESS-WIFI-TEMPERATURE.ino
@@ -12,8 +12,8 @@ const char* password = "123456789";
 #define DHTPIN 5     // Digital pin connected to the DHT sensor
 
 // Uncomment the type of sensor in use:
-//#define DHTTYPE    DHT11     // DHT 11
-#define DHTTYPE    DHT22     // DHT 22 (AM2302)
+#define DHTTYPE    DHT11     // DHT 11
+//#define DHTTYPE    DHT22     // DHT 22 (AM2302)
 //#define DHTTYPE    DHT21     // DHT 21 (AM2301)
 
 DHT dht(DHTPIN, DHTTYPE);
