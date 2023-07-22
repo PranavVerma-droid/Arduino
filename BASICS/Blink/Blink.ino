c8549bb7553875b81b7b78db9c536f1c3d46dc41
Pranav Verma <pranav@verma.net.in>
2023-07-22 16:18:10 +0530

    Removed Something

--- BASICS/Blink/Blink.ino
+++ BASICS/Blink/Blink.ino
@@ -1,20 +1,12 @@
-int led = 43;
 void setup() {
   pinMode(LED_BUILTIN,OUTPUT);
-  pinMode(led,OUTPUT);
 }
 void loop() {
   digitalWrite(LED_BUILTIN,HIGH);
-  
-  digitalWrite(led,HIGH);
 
   delay(1000);
 
   digitalWrite(LED_BUILTIN,LOW);
-  
-  digitalWrite(led,LOW);
-  //delay adding fo
 
   delay(1000);
-  //helpful for many reasons ofc llol
 }
